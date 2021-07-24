d2530af139c3f6f499dd2bb2f856d33cad5df0c1
abdelhamidfg <abdelhamidfg@gmail.com>
2021-07-25 00:12:01 +0400

    Update hamid_policy.lua

--- policies/hamid-policy/1.0.0/hamid_policy.lua
+++ policies/hamid-policy/1.0.0/hamid_policy.lua
@@ -77,7 +77,7 @@ if not res then
 end
   if res then
       ngx.log(ngx.ERR, "request success: ", res.body)
-      if not isempty(res.body) and string.find(res.body, "true") then
+      if not isempty(res.body) and string.find(res.body, "Kamarhati") then
           return true
       end
   end      
