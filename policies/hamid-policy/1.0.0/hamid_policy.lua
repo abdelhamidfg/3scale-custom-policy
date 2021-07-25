49b3113413c6af79c8e1bcff435b8ef7631bf11a
abdelhamidfg <abdelhamidfg@gmail.com>
2021-07-25 22:59:38 +0400

    Update hamid_policy.lua

--- policies/hamid-policy/1.0.0/hamid_policy.lua
+++ policies/hamid-policy/1.0.0/hamid_policy.lua
@@ -7,42 +7,7 @@ local insert = table.insert
 
 function _M.new(configuration)
   local self = new()
-
-
- local ops = {}
- local httpc = require("resty.http").new()
- local res, err = httpc:request_uri("http://location-service-dil-proj2.apps.cluster.ocp-hamid.com/locations", {
-    method = "GET",
-    body = "",
-    headers = {
-        ["Content-Type"] = "application/json",
-    },
-})
-if not res then
-    ngx.log(ngx.ERR, "request failed: ", err)
-    -- return
-end
-  --if res then
-    --  ngx.log(ngx.ERR, "request success: ", res.body)
-  --end   
-
--- At this point, the entire request / response is complete and the connection
--- will be closed or back on the connection pool.
-
--- The `res` table contains the expeected `status`, `headers` and `body` fields.
-local status = res.status
-local length = res.headers["Content-Length"]
-local body   = res.body 
-  local config = configuration or {}
-  local set_header = config.set_header or {}
-  
-  for _, header in ipairs(set_header) do
-    insert(ops, function()
-      ngx.log(ngx.ERR, 'setting header V1: ', x, ' to: ', header.value)
-      ngx.req.set_header(x, header.value)
-    end)
-  end
-
+  local ops = {}
   self.ops = ops
   self.author_rest_endpoint=config.author_rest_endpoint
   self.JWT_claim_name=config.JWT_claim_name
@@ -75,11 +40,11 @@ local function check_authorization(auth_endpoint,role,method,resource)
         },
       })
 if not res then
-    ngx.log(ngx.ERR, "request failed: ", err)
+    ngx.log(ngx.ERR, "Authorization service request failed: ", err)
     return is_authorized 
 end
   if res then
-      ngx.log(ngx.ERR, "request success: ", res.body)
+      ngx.log(ngx.ERR, "Authorization service request success: ", res.body)
       if not isempty(res.body) and string.find(res.body, "true") then
           return true
       end
@@ -93,32 +58,11 @@ local function deny_request(error_msg)
   ngx.exit(ngx.status)
 end
 
-function _M:rewrite()
-ngx.log(ngx.ERR,'rewrite start')
-  for _,op in ipairs(self.ops) do
-    op()
-  end
-end
+
 function _M:access(context)
-  ngx.log(ngx.ERR,'access start')
-  ngx.log(ngx.ERR,"self.JWT_claim_name=",self.JWT_claim_name)
-  ngx.log(ngx.ERR,"self.JWT_claim_name value=",context.jwt[self.JWT_claim_name])
-    local uri = ngx.var.uri
+  local uri = ngx.var.uri
   local request_method =  ngx.req.get_method()
   local is_auth=check_authorization( self.author_rest_endpoint,context.jwt[self.JWT_claim_name],request_method,uri)
-  ngx.log(ngx.ERR, "is_auth= ", is_auth)
-  --local uri = context:get_uri()
-
-  --ngx.log(ngx.ERR, "context.jwt= ", context.jwt)
-  if context.jwt then
-     print("esthaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-  end
-  ngx.log(ngx.ERR, "context.jwt.user_group= ", context.jwt.user_group)
-  
-  ngx.log(ngx.ERR, "uri=: ", uri)
-  ngx.log(ngx.ERR, "type of request_method= ", type(request_method))
-  ngx.log(ngx.ERR, "request_method=: ", request_method)
-  ngx.log(ngx.ERR, "client=: ", request_method.client)
   if  is_auth == false then
    return deny_request(self.error_message)
   end   
